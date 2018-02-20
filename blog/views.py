fee6ebd88e29e0c717b6814e6b2926eb80920fed
rgroener <username@users.noreply.github.com>
2018-02-20 09:38:45 +0000

    Template Inheritance in Django

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 import datetime
 from django.shortcuts import render
+from django.conf import settings
 
 def index(request):
 		return HttpResponse("Hello Django")
 
 def today_is(request):
 		now = datetime.datetime.now()
-		return render(request, 'blog/datetime.html', {'now': now})
+		return render(request, 'blog/datetime.html', {
+								'now': now, 
+								'template_name': 'blog/nav.html',
+								'base_dir': settings.BASE_DIR}
+							)
