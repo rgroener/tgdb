236c18f7e3b77b9727a03373451d983158858275
rgroener <username@users.noreply.github.com>
2018-02-22 06:43:10 +0000

    Building Blog The First Steps

--- blog/models.py
+++ blog/models.py
@@ -7,16 +7,26 @@ class Author(models.Model):
 	created_on = models.DateTimeField(auto_now_add=True)
 	last_logged_in = models.DateTimeField(auto_now=True)
 	
+	def __str__(self):
+		return self.name + " : " + self.email
+	
 	
 class Category(models.Model):
 	name = models.CharField(max_length=100, unique=True)
 	slug = models.SlugField(max_length=100, unique=True)
 	author = models.ForeignKey(Author)
 	
+	def __str__(self):
+		return self.name
+	
 class Tag(models.Model):
 	name = models.CharField(max_length=100, unique=True)
 	slug = models.SlugField(max_length=100, unique=True)
 	author = models.ForeignKey(Author)
+	
+	def __str__(self):
+		return self.name
+	
 	
 class Post(models.Model):
 	title = models.CharField(max_length=200)
@@ -26,4 +36,7 @@ class Post(models.Model):
 	author = models.ForeignKey(Author)
 	category = models.ForeignKey(Category)
 	tags = models.ManyToManyField(Tag)
+	
+	def __str__(self):
+		return self.title
 	
