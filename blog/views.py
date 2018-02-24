a49b6ee9f9b1808bcc8629f781e70472bf482cd5
rgroener <username@users.noreply.github.com>
2018-02-24 13:00:37 +0000

    showing 404 errors in Django without SEO friendly URL's(

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,33 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Author, Tag, Category, Post
 
 def index(request):
 		return HttpResponse("Hello Django")
 
 def post_list(request):
-	posts = Post.objects.all()
+	posts = Post.objects.order_by("-id").all()
 	return render(request, 'blog/post_list.html', {'posts': posts})
 	
 def post_detail(request, pk):    
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
     
 # view function to display post by category
 def post_by_category(request, category_slug):
-    category = Category.objects.get(slug=category_slug)
-    posts = Post.objects.filter(category__slug=category_slug)
+    category = get_object_or_404(Category, slug=category_slug)
+    posts = get_list_or_404(Post.objects.order_by("-id"), category__slug=category_slug)
     context = {
         'category': category,
         'posts': posts
     }
-    print(category)
     return render(request, 'blog/post_by_category.html', context)
 
 
 # view function to display post by tag
 def post_by_tag(request, tag_slug):
-    tag = Tag.objects.get(slug=tag_slug)
-    posts = Post.objects.filter(tags__name=tag)
+    tag = get_object_or_404(Tag, slug=tag_slug)
+    posts = get_list_or_404(Post.objects.order_by("-id"), tags__name=tag)
     context = {
         'tag': tag,
         'posts': posts
