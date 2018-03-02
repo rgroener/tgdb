3ebe2f6fa702992c0b2ac8182dcaa1381fd28804
rgroener <username@users.noreply.github.com>
2018-03-02 15:24:27 +0000

    minor changes

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Author, Tag, Category, Post
 
@@ -9,9 +9,9 @@ def post_list(request):
 	posts = Post.objects.order_by("-id").all()
 	return render(request, 'blog/post_list.html', {'posts': posts})
 	
-def post_detail(request, pk):    
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+def post_detail(request, pk): 
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
     
 # view function to display post by category
 def post_by_category(request, category_slug):
