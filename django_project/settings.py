6507397fdb624669d28451578eee1aa04b7cfc3d
rgroener <username@users.noreply.github.com>
2018-03-04 17:06:55 +0000

    handling static content in django completed

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,7 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+## STATIC FILES ##
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),    
+]
 
 # Email settings
 
